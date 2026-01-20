6296495a9161fcf928dd7ebb2ef452d7e504df3c
lilia <3931448727@qq.com>
2026-01-20 18:14:29 +0800

    asio

---
+++ build/CMakeFiles/MyPrivLib.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyPrivLib.dir/src/lib/college.cpp.o"
+  "CMakeFiles/MyPrivLib.dir/src/lib/college.cpp.o.d"
+  "CMakeFiles/MyPrivLib.dir/src/lib/course.cpp.o"
+  "CMakeFiles/MyPrivLib.dir/src/lib/course.cpp.o.d"
+  "CMakeFiles/MyPrivLib.dir/src/lib/functions.cpp.o"
+  "CMakeFiles/MyPrivLib.dir/src/lib/functions.cpp.o.d"
+  "CMakeFiles/MyPrivLib.dir/src/lib/mankind.cpp.o"
+  "CMakeFiles/MyPrivLib.dir/src/lib/mankind.cpp.o.d"
+  "libMyPrivLib.a"
+  "libMyPrivLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyPrivLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
